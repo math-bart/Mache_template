7a4884584ad67e132542455aeff473a27a64dc47
Bartosz Wojtaś <el.klides@op.pl>
2018-05-31 13:41:16 +0200

    Fix modals buttons

--- js/scripts.js
+++ js/scripts.js
@@ -40,15 +40,19 @@ $(function(){
   });
   $('#submit-prices-s').click(function() {
     $('#body-prices-s').html('<h2>DONE!</h2><h5>Thank you for choose us! Transaction details was send to Your e-mail</h5>');
+	$('#submit-prices-s').css('display', 'none');
   });
   $('#submit-prices-b').click(function() {
     $('#body-prices-b').html('<h2>DONE!</h2><h5>Thank you for choose us! Transaction details was send to Your e-mail</h5>');
+	$('#submit-prices-b').css('display', 'none');
   });
    $('#submit-prices-p').click(function() {
     $('#body-prices-p').html('<h2>DONE!</h2><h5>Thank you for choose us! Transaction details was send to Your e-mail</h5>');
+	$('#submit-prices-p').css('display', 'none');
   });
   $('#submit-prices-u').click(function() {
     $('#body-prices-u').html('<h2>DONE!</h2><h5>Thank you for choose us! Transaction details was send to Your e-mail</h5>');
+	$('#submit-prices-u').css('display', 'none');
   });
   $('#submit-contact').click(function() {
     $('#body-contact').html("<h2>DONE!</h2><h5>Thank you for Tour message! We anserw as soon as it's possible</h5>");
